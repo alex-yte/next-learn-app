071f2b141cf43f9887136ad2057a8ffaeb36faa9
Denis <den.alex.chirkov@gmail.com>
2025-02-12 00:13:19 +0100

    feat: add navigation component

---
+++ app/components/menu.tsx
@@ -0,0 +1,21 @@
+//navigation menu component
+import Link from 'next/link';
+
+export default function Menu() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about">
+                        <a>About</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
