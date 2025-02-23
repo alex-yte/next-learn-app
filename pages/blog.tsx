395114fab89c6da6fda2188c30c4377e8938dc36
Denis <den.alex.chirkov@gmail.com>
2025-02-23 22:53:23 +0100

    fix: add authors to blog cards

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,44 +1,68 @@
-import type { InferGetStaticPropsType, GetStaticProps } from "next";
-import PostCard from "./components/PostCard";
-
+// pages/blog.tsx
+import type { InferGetStaticPropsType, GetStaticProps } from 'next';
+import PostCard from './components/PostCard';
 import './blog.css';
+import { log } from 'console';
 
 type Post = {
   userId: number;
   id: number;
   title: string;
   body: string;
-}
+};
 
+type Author = {
+  id: number;
+  name: string;
+};
+
+type PostWithAuthor = Post & { authorName: string };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5&_page=1")
-  const posts = await res.json();
+  // Получение постов
+  const resPosts = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=7&_page=2');
+  const posts: Post[] = await resPosts.json();
+  console.log(posts);
+
+  // Получение авторов
+  const resAuthors = await fetch('https://jsonplaceholder.typicode.com/users');
+  const authors: Author[] = await resAuthors.json();
+  console.log(authors);
+
+  // Объединение постов с именами авторов
+  const postsWithAuthors: PostWithAuthor[] = posts.map((post) => {
+    const author = authors.find((author) => author.id === post.userId);
+    return {
+      ...post,
+      authorName: author ? author.name : 'Неизвестный автор',
+    };
+  });
+  console.log(postsWithAuthors);
 
 
   return {
     props: {
-      posts,
+      posts: postsWithAuthors,
     },
   };
 };
 
 export default function BlogPage({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
-
-    return (
-      <main>
-        <h1>Blog</h1>
-        <ul id="blog-list">
-          {posts.map((post: Post) => (
-            <PostCard
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              body={post.body}
-              link={`/blog/${post.id}`}
-            />
-          ))}
-        </ul>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>Блог</h1>
+      <ul id="blog-list">
+        {posts.map((post: PostWithAuthor) => (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            body={post.body}
+            link={`/blog/${post.id}`}
+            authorName={post.authorName}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+}
--- pages/components/PostCard.tsx
+++ pages/components/PostCard.tsx
@@ -1,19 +1,23 @@
-// components/PostCard.js
+// components/PostCard.tsx
 import React from 'react';
 
 interface PostCardProps {
-    id : number;
-    title: string;
-    body: string;
-    link: string;
+  id: number;
+  title: string;
+  body: string;
+  link: string;
+  authorName: string;
 }
 
-const PostCard: React.FC<PostCardProps> = ({ id, title, body, link }) => (
+const PostCard: React.FC<PostCardProps> = ({ id, title, body, link, authorName }) => {
+  return (
     <li key={id}>
-    <h2>{title}</h2>
-    <p>{body}</p>
-    <a href={link}>Read more</a>
-  </li>
-);
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <h3>{authorName}</h3>
+      <a href={link}>Читать далее</a>
+    </li>
+  );
+};
 
 export default PostCard;
