b25eb3d62133365ad63e2245f9d8079354a7bf18
Denis <den.alex.chirkov@gmail.com>
2025-04-18 12:31:04 +0200

    feat: new navigation items and scrollbar customization

--- pages/components/menu.tsx
+++ pages/components/menu.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Menu() {
     return (
         <nav>
-            <ul  className='flex justify-center gap-[1rem]' >
+            {/* <ul  className='flex justify-center gap-[1rem]' >
                 <li>
                     <Link className='underline' href="/">
                         Home
@@ -24,6 +24,21 @@ export default function Menu() {
                     <Link className='underline' href="/contact">
                         Contacts
                     </Link>
+                </li>
+
+            </ul> */}
+            <ul className="menu flex justify-center gap-[1rem]">
+                <li className="menu_item">
+                    <Link href="#hero">Home</Link>
+                </li>
+                <li className="menu_item">
+                    <Link href="#about">About</Link>
+                </li>
+                <li className="menu_item">
+                    <Link href="#projects">Projects</Link>
+                </li>
+                <li className="menu_item">
+                    <Link href="#contacts">Contacts</Link>
                 </li>
             </ul>
         </nav>
